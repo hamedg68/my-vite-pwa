d3f0112d02a08bfa4fb932bbed28ceef0d18ae97
hamed <hamedgorji1368@gmail.com>
2022-05-21 08:23:22 +0430

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  base: "/my-vite-pwa/",
+  plugins: [
+    vue(),
+    VitePWA({
+     
+    }),
+  ],
+});
